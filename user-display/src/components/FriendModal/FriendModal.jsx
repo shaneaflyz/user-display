4ce49850efaf93ff4c320370ea66fffb04b9ce03
Shane Aflyz <shaneaflyz@gmail.com>
2024-08-04 17:15:14 +0800

    beautify close button

--- user-display/src/components/FriendModal/FriendModal.jsx
+++ user-display/src/components/FriendModal/FriendModal.jsx
@@ -7,15 +7,15 @@ const FriendModal = ({ show, onClose, friend }) => {
   }
 
   return (
-    <div className="modal-overlay">
-      <div className="modal">
+    <div className="friend-modal-overlay">
+      <div className="friend-modal">
         <img src={friend.picture.medium} alt={`${friend.name.first} ${friend.name.last}`} />
         <h2>{friend.name.first} {friend.name.last}</h2>
         <p>Email: {friend.email}</p>
         <p>Phone: {friend.phone}</p>
         <p>Date of Birth: {friend.dob.date.toString()}</p>
         <p>Location: {friend.location.city}, {friend.location.country}</p>
-        <button onClick={onClose}>Close</button>
+        <button className='button' onClick={onClose}>Close</button>
       </div>
     </div>
   )
